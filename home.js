373a9fa87ea958146da64e31c05360e28ff80c21
Zhakebai <110485812+Zhakebai@users.noreply.github.com>
2023-11-25 04:31:22 +0600

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
-
+var sound1= new Audio();
+sound1.src = "click.wav";
 
 const headerBg = () =>{
     const header = document.querySelector(".js-header");
@@ -13,4 +14,20 @@ const headerBg = () =>{
     })
 }
 
+const image = document.getElementById('shakeImage');
+
+    function shakeImage() {
+      
+      image.classList.add('shake');
+
+      setTimeout(() => {
+        image.classList.remove('shake');
+      }, 500);
+    }
+    
+    const shakeButton = document.createElement('button');
+    shakeButton.textContent = 'Shake Image';
+    shakeButton.addEventListener('click', shakeImage);
+
+    document.body.appendChild(shakeButton);
 
